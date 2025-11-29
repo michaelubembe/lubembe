23f573c55fab0d0fb00f7a457029b38b9aaa22e8
Lubembe Michael <lubemb@gmail.com>
2025-11-29 17:37:00 +0300

    Fix Vercel build by refactoring sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,12 +3,15 @@ import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas/index'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || ''
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+
 export default defineConfig({
     name: 'default',
     title: 'Lubembe Blog',
 
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+    projectId,
+    dataset,
 
     basePath: '/studio',
 
